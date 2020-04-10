5a57537460e3f2fd1a182ad55e02027b08140bca
Junha Park <jpark3@scu.edu>
2020-04-09 18:17:09 -0700

    Added animated loading spinner

--- ui/src/components/LoadingScreen.tsx
+++ ui/src/components/LoadingScreen.tsx
@@ -1,12 +1,16 @@
 import React, {
   Component
 } from 'react';
+import { Spinner } from 'react-bootstrap';
+import './LoadingScreen.css'
 
 export default class LoadingScreen extends Component {
   render () {
     return (
-      <div>
-        <h2>Loading...</h2>
+      <div className='centered'>
+        <Spinner animation='border' variant='light'>
+          <span className='sr-only'>Loading...</span>
+        </Spinner>
       </div>
     );
   };
