0f330a00f88a493cb3e2e058806c06288ba6950a
Junha Park <jpark3@scu.edu>
2020-05-12 21:05:29 -0700

    API now queries from MongoDB

--- api/api.py
+++ api/api.py
@@ -4,7 +4,13 @@ from flask_restful import Resource, Api
 from flask_cors import CORS
 from werkzeug.exceptions import BadRequest, NotFound
 
-import pymongo
+# MongoDB
+from pymongo import MongoClient
+from pymongocreds import CONN_STR
+
+client = MongoClient(CONN_STR)
+db = client['personalwebsite']
+col = db['PersonalWebsite']
 
 # App
 app = Flask(__name__)
@@ -15,18 +21,23 @@ app.config['DEBUG'] = True
 api = Api(app)
 
 # Resources
-class ServerStatus(Resource):
+class All(Resource):
     def get(self):
         response = {}
+        items = []
         try:
-            response['message'] = 'Server is running.'
+            for result in col.find():
+                result['_id'] = str(result['_id'])
+                items.append(result)
+
+            response['message'] = 'Request successful.'
+            response['result'] = items
             return response, 200
         except:
-            response['message'] = 'Internal server error.'
-            return response, 500
+            raise BadRequest('Request failed, please try again later.')
 
 # Routes
-api.add_resource(ServerStatus, '/')
+api.add_resource(All, '/')
 
 if __name__ == '__main__':
- app.run(host = 'localhost', port = 3080)
+    app.run(host='localhost', port=3080)
