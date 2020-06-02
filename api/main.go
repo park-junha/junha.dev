4146a6c45deab0c043ba9108a75ab0e2fb20896c
Junha Park <jpark3@scu.edu>
2020-06-02 16:13:14 -0700

    1.8 subtopic branch for GraphQL API

--- api/main.go
+++ api/main.go
@@ -2,8 +2,23 @@ package main
 
 import (
         "fmt"
+        "net/http"
+        "encoding/json"
+
+	"github.com/friendsofgo/graphiql"
+	"github.com/graphql-go/graphql"
 )
 
 func main() {
-        fmt.Println("New GraphQL API is currently R&D - come back soon!")
+	http.Handle("/graphql", gqlHandler())
+	http.ListenAndServe(":2000", nil)
+
+        // Graphiql
+	graphiqlHandler, err := graphiql.NewGraphiqlHandler("/graphql")
+	if err != nil {
+		panic(err)
+	}
+	http.Handle("/graphiql", graphiqlHandler)
+
+        fmt.Println("Serving GraphQL API.")
 }
