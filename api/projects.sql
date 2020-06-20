190849eb24b467c07cbcb535125906b17cdb44b2
Junha Park <jpark3@scu.edu>
2020-06-20 00:04:02 -0700

    Project data query

---
+++ api/projects.sql
@@ -0,0 +1,35 @@
+SELECT
+	project_id
+	, title
+	, description
+	, about
+	, url
+	, source_code_url
+	, ARRAY(
+	SELECT
+		ROW_TO_JSON(ls)
+	FROM (
+		SELECT
+			l.*
+		FROM
+			UNNEST(p.languages) project_langs
+		LEFT JOIN
+			tools l
+		ON
+			l.tool_id = project_langs
+	) ls)
+	, ARRAY(
+	SELECT
+		ROW_TO_JSON(ots)
+	FROM (
+		SELECT
+			ot.*
+		FROM
+			UNNEST(p.other_tools) project_tools
+		LEFT JOIN
+			tools ot
+		ON
+			ot.tool_id = project_tools
+	) ots)
+FROM
+	projects p;
