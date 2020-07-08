b916724ac2e3711785cae9d55773d5572bc50627
Junha Park <jpark3@scu.edu>
2020-07-07 23:26:51 -0700

    Added navbar and basic styling

--- ng/src/app/app.component.ts
+++ ng/src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface NavRoutes extends Array<NavRoute>{};
+interface NavRoute {
+  label: string;
+  route: string;
+};
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private router: Router) { }
+
   title = 'junha-angular';
+  home: string = '/';
+
+  appRoutes: NavRoutes = [
+    {
+      'label': 'About',
+      'route': '/about'
+    },
+    {
+      'label': 'Experience',
+      'route': '/experience'
+    },
+    {
+      'label': 'Projects',
+      'route': '/projects'
+    }
+  ];
+
+  routeIsActive = (route: string): boolean => {
+    if (this.router.url === route) {
+      return true;
+    }
+    return false;
+  };
+
+  destinationRoute = (route: string): string => {
+    return this.routeIsActive(route) ? this.home : route;
+  };
 }
