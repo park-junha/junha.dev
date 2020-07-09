333b435c3d0a5eed0bde376ce8b615a0636d1c6b
Junha Park <jpark3@scu.edu>
2020-07-08 19:20:50 -0700

    Added proper styling to app and about

--- ng/src/app/about/about.component.ts
+++ ng/src/app/about/about.component.ts
@@ -25,7 +25,11 @@ export class AboutComponent {
 
   contents: AboutContents = {
     'title': 'Hi. My name is Junha Park.',
-    'intro': 'I\'m a software engineer based in the Bay Area.',
+    'intro': 'I\'m a software engineer based in the Bay Area specializing \
+      in Full Stack Development. My primary technologies are JavaScript \
+      and its various flavors and frameworks, including React, Angular, \
+      TypeScript, and Node.js. Outside of the JavaScript ecosystem, I also \
+      use Python, SQL, and Go.',
     'interests': {
       'heading': 'These are some of my interests and hobbies:',
       'items': [
