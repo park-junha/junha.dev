309be48bb6123dfffcd37ef8f2538102df8133d4
Junha Park <jpark3@scu.edu>
2020-07-08 17:50:14 -0700

    Added About content

--- ng/src/app/about/about.component.ts
+++ ng/src/app/about/about.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 
+interface AboutContents {
+  title: string;
+  intro: string;
+  interests: Interests;
+};
+interface Interests {
+  heading: string;
+  items: Array<Interest>;
+};
+interface Interest {
+  name: string;
+  url?: string;
+};
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  contents: AboutContents = {
+    'title': 'Hi. My name is Junha Park.',
+    'intro': 'I\'m a software engineer based in the Bay Area.',
+    'interests': {
+      'heading': 'These are some of my interests and hobbies:',
+      'items': [
+        {
+          'name': 'Open source software',
+          'url': 'https://github.com/park-junha'
+        },
+        {
+          'name': 'Listening to music'
+        },
+        {
+          'name': 'Fashion'
+        },
+        {
+          'name': 'Twisty cube puzzles',
+          'url': 'https://www.worldcubeassociation.org/persons/2015PARK22'
+        },
+        {
+          'name': 'Video games'
+        }
+      ]
+    }
+  };
 }
