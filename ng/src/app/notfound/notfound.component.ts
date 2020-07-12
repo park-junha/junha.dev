fb82d4016b01a744ce61ee2dda1b688065b096cb
Junha Park <jpark3@scu.edu>
2020-07-11 23:44:31 -0700

    Polished Not Found

--- ng/src/app/notfound/notfound.component.ts
+++ ng/src/app/notfound/notfound.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notfound',
   templateUrl: './notfound.component.html',
   styleUrls: ['./notfound.component.css']
 })
-export class NotfoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class NotfoundComponent { }
