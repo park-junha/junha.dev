1836e848f72ccd81a5227de0d7e39dacd69fb958
Junha Park <jpark3@scu.edu>
2020-07-13 12:45:24 -0700

    Added Experience queries to deploy script

--- api/deploy.sh
+++ api/deploy.sh
@@ -3,6 +3,11 @@ BASE_DIR="$(cd "$(dirname "$0" )" && pwd )"
 TEMP_DIR=/tmp/main
 TEMPZIP_DIR=$TEMP_DIR.zip
 
+PROJECTS_SQL=$BASE_DIR/projects.sql
+PROJECT_SQL=$BASE_DIR/project.sql
+EXPERIENCES_SQL=$BASE_DIR/experiences.sql
+EXPERIENCE_SQL=$BASE_DIR/experience.sql
+
 # LAMBDA_FUNCTION required in .env file
 LAMBDA_FUNCTION=$(grep LAMBDA_FUNCTION $BASE_DIR/.env | cut -d '=' -f2)
 
@@ -10,7 +15,9 @@ cd $BASE_DIR
 echo "Building app to $TEMP_DIR..."
 env GOOS=linux GOARCH=amd64 go build -o $TEMP_DIR
 echo "Zipping app to $TEMPZIP_DIR..."
-zip -j $TEMPZIP_DIR $TEMP_DIR $BASE_DIR/projects.sql $BASE_DIR/project.sql
+zip -j $TEMPZIP_DIR $TEMP_DIR $PROJECTS_SQL $PROJECT_SQL $EXPERIENCES_SQL \
+    $EXPERIENCE_SQL
 echo "Deploying your app to Lambda function $LAMBDA_FUNCTION..."
-aws lambda update-function-code --function-name $LAMBDA_FUNCTION --zip-file fileb://$TEMPZIP_DIR
+aws lambda update-function-code --function-name $LAMBDA_FUNCTION \
+    --zip-file fileb://$TEMPZIP_DIR
 echo "Done!"
