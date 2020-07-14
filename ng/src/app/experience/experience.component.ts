d1efe7cc20e59b2470a3960fda5cab27b7360646
Junha Park <jpark3@scu.edu>
2020-07-13 19:14:35 -0700

    Added accordion to experience section

--- ng/src/app/experience/experience.component.ts
+++ ng/src/app/experience/experience.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { ApiService, Experience } from '../api.service';
+import { RIPPLE_COLOR_RED } from '../../environments/constants';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.css']
 })
-export class ExperienceComponent implements OnInit {
+export class ExperienceComponent {
+  experiences: Array<Experience>;
+  _subscription: Subscription;
+  rippleColor: string = RIPPLE_COLOR_RED;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private apiService: ApiService) {
+    this.experiences = this.apiService.getExperiences();
+    this._subscription = apiService.apiUpdate.subscribe(updated => {
+      this.experiences = updated.experiences;
+    });
   }
 
+  private formatDate(date: string): string {
+    return new Date(date).toLocaleString('default', {
+      month: 'short',
+      year: 'numeric'
+    });
+  }
+
+  getDateRange(experience: Experience): string {
+    let start: string = this.formatDate(experience.start_date)
+    let end: string = (experience.end_date === 'Current' ?
+      'Current' : this.formatDate(experience.end_date));
+    return `${start} - ${end}`;
+  }
 }
