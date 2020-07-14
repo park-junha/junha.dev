3bb107c627699303466fd3383f4c2cf858d36fa3
Junha Park <jpark3@scu.edu>
2020-07-13 20:42:40 -0700

    Footer with GitHub, LinkedIn, and source code links

--- ng/src/environments/constants.ts
+++ ng/src/environments/constants.ts
@@ -1,3 +1,5 @@
+export const VERSION: string = '3.0.0';
+
 export const RIPPLE_COLOR_RED: string = 'rgba(215, 80, 80, 0.15)';
 export const FETCH_ALL_QUERY: string = '{ experiences { experience_id \
   label company title start_date end_date description } projects { \
