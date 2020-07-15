2f36004c52e3bc4a8e3944de3064129a3a81c349
Junha Park <jpark3@scu.edu>
2020-07-14 17:44:15 -0700

    Added tech queries to AWS Lambda deploy script

--- api/deploy.sh
+++ api/deploy.sh
@@ -7,6 +7,8 @@ PROJECTS_SQL=$BASE_DIR/projects.sql
 PROJECT_SQL=$BASE_DIR/project.sql
 EXPERIENCES_SQL=$BASE_DIR/experiences.sql
 EXPERIENCE_SQL=$BASE_DIR/experience.sql
+TECHS_SQL=$BASE_DIR/techs.sql
+TECH_SQL=$BASE_DIR/tech.sql
 
 # LAMBDA_FUNCTION required in .env file
 LAMBDA_FUNCTION=$(grep LAMBDA_FUNCTION $BASE_DIR/.env | cut -d '=' -f2)
@@ -16,7 +18,7 @@ echo "Building app to $TEMP_DIR..."
 env GOOS=linux GOARCH=amd64 go build -o $TEMP_DIR
 echo "Zipping app to $TEMPZIP_DIR..."
 zip -j $TEMPZIP_DIR $TEMP_DIR $PROJECTS_SQL $PROJECT_SQL $EXPERIENCES_SQL \
-    $EXPERIENCE_SQL
+    $EXPERIENCE_SQL $TECHS_SQL $TECH_SQL
 echo "Deploying your app to Lambda function $LAMBDA_FUNCTION..."
 aws lambda update-function-code --function-name $LAMBDA_FUNCTION \
     --zip-file fileb://$TEMPZIP_DIR
