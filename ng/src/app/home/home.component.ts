e6f4cd68eb1a197cac2edf5afc12a90afd591b54
Junha Park <jpark3@scu.edu>
2020-07-17 20:06:55 -0700

    Removed extraneous code in Home Component

--- ng/src/app/home/home.component.ts
+++ ng/src/app/home/home.component.ts
@@ -1,15 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  template: '',
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class HomeComponent { }
