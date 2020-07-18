80c77b998c6c07642d0337eaed420a934b56071e
Junha Park <jpark3@scu.edu>
2020-07-18 15:41:38 -0700

    Added dummy data file for usage by unit tests

--- ng/src/app/experience/experience.component.spec.ts
+++ ng/src/app/experience/experience.component.spec.ts
@@ -14,51 +14,12 @@ import {
 import { ExperienceComponent } from './experience.component';
 import { ApiService } from '../api.service';
 
+import dummyData from '../../test.data';
+
 describe('ExperienceComponent', () => {
   let component: ExperienceComponent;
   let service: ApiService;
   let fixture: ComponentFixture<ExperienceComponent>;
-
-  const dummyData = {
-    'experiences': [
-      {
-        'label': 'Google',
-        'company': 'Google',
-        'title': 'Software Engineer',
-        'start_date': '2019-01-01T00:00:00Z',
-        'end_date': 'Current',
-        'description': [
-          'Full Stack Software Engineer crushin it'
-        ]
-      },
-      {
-        'label': 'IBM',
-        'company': 'IBM',
-        'title': 'Software Engineer',
-        'start_date': '2016-01-01T00:00:00Z',
-        'end_date': '2018-12-31T00:00:00Z',
-        'description': [
-          'BigFix is no longer owned by IBM'
-        ]
-      }
-    ],
-    'projects': [
-      {
-        'title': 'V8',
-        'languages': [
-          {
-            'name': 'C++',
-            'color': '#f34b7d'
-          }
-        ],
-        'tools': [],
-        'description': 'JavaScript engine by Google',
-        'about': 'Runs on Chrome and Node.js',
-        'url': '',
-        'source_code_url': ''
-      }
-    ]
-  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
---
+++ ng/src/test.data.ts
@@ -0,0 +1,69 @@
+const dummyData = {
+  'experiences': [
+    {
+      'label': 'Google',
+      'company': 'Google',
+      'title': 'Software Engineer',
+      'start_date': '2019-01-01T00:00:00Z',
+      'end_date': 'Current',
+      'description': [
+        'Full Stack Software Engineer crushin it'
+      ]
+    },
+    {
+      'label': 'IBM',
+      'company': 'IBM',
+      'title': 'Software Engineer',
+      'start_date': '2016-01-01T00:00:00Z',
+      'end_date': '2018-12-31T00:00:00Z',
+      'description': [
+        'BigFix is no longer owned by IBM'
+      ]
+    }
+  ],
+  'projects': [
+    {
+      'title': 'V8',
+      'languages': [
+        {
+          'name': 'C++',
+          'color': '#f34b7d'
+        }
+      ],
+      'tools': [],
+      'description': 'JavaScript engine by Google',
+      'about': 'Runs on Chrome and Node.js',
+      'url': '',
+      'source_code_url': ''
+    },
+    {
+      'title': 'Personal Website',
+      'languages': [
+        {
+          'name': 'TypeScript',
+          'color': '#286c7f'
+        },
+        {
+          'name': 'Go',
+          'color': '#00add8'
+        }
+      ],
+      'tools': [
+        {
+          'name': 'Angular',
+          'color': '#a3112d'
+        },
+        {
+          'name': 'GraphQL',
+          'color': '#e535ab'
+        }
+      ],
+      'description': 'This site',
+      'about': 'Written in Angular with a GraphQL backend',
+      'url': 'https://junha.dev',
+      'source_code_url': 'https://github.com/park-junha/PersonalWebsite'
+    }
+  ]
+};
+
+export default dummyData;
