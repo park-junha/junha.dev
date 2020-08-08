664671561cc0e8c521fc283031d1d196f607be37
Junha Park <jpark3@scu.edu>
2020-08-08 16:17:36 -0700

    Update resume 8/8/2020

--- legacy/src/components/Portfolio/Resume.tsx
+++ legacy/src/components/Portfolio/Resume.tsx
@@ -115,8 +115,8 @@ class Resume extends Component<Props> {
               </ul>
               <li>Web Backend</li>
               <ul className='resume-text'>
-                <li>Flask</li>
                 <li>Express</li>
+                <li>Flask</li>
               </ul>
               <li>API Protocols</li>
               <ul className='resume-text'>
@@ -135,6 +135,7 @@ class Resume extends Component<Props> {
               <li>NoSQL</li>
               <ul className='resume-text'>
                 <li>MongoDB</li>
+                <li>DynamoDB</li>
               </ul>
             </ul>
             <h6><strong>DevOps</strong></h6>
@@ -143,52 +144,51 @@ class Resume extends Component<Props> {
               <ul className='resume-text'>
                 <li>AWS</li>
               </ul>
-              <li>Automation</li>
+              <li>Test Automation</li>
               <ul className='resume-text'>
-                <li>Jasmine</li>
                 <li>Jenkins</li>
+                <li>Jasmine / Jest</li>
                 <li>Protractor</li>
               </ul>
               <li>Git</li>
-              <li>Scrum</li>
             </ul>
           </div>
           <div className='resume-body-right'>
             <h5 className='resume-body-header'>Professional Experience</h5>
             <h6>
-              <strong>Software Engineer</strong>, HCL BigFix
-              <span className='resume-date-range'>5/20 - 8/20</span>
+              <strong>Full Stack Software Engineer</strong>, HCL BigFix
+              <span className='resume-date-range'>5/20 - Now</span>
             </h6>
             <ul className='resume-text'>
               <li><i>Software Engineer</i> for the web platform of a large <a href='https://www.hcltechsw.com/products/bigfix' target='_blank' rel='noopener noreferrer'>endpoint manager</a>.</li>
               <li><strong>Languages</strong>: Implemented with <strong>Node.js</strong>, <strong>JavaScript</strong>, and <strong>TypeScript</strong>.</li>
               <li><strong>Web</strong>: Launched several <strong>Angular</strong> web user interfaces and internal <strong>Express</strong> REST APIs.</li>
-              <li><strong>Database</strong>: Integrated and developed several <strong>SQL Server</strong> and <strong>IBM DB2</strong> queries.</li>
+              <li><strong>Databases</strong>: Integrated and developed several <strong>SQL Server</strong> and <strong>IBM Db2</strong> queries.</li>
               <li><strong>Automation</strong>: Created end-to-end and unit test cases with <strong>Jasmine</strong> and <strong>Protractor</strong>.</li>
               <li><strong>Development</strong>: Monitored builds on <strong>Jenkins</strong> and delivered release items to the customer.</li>
             </ul>
             <h6>
-              <strong>Software Team Lead</strong>, Infinite Options
+              <strong>Full Stack Software Engineer</strong>, Infinite Options
               <span className='resume-date-range'>9/19 - 5/20</span>
             </h6>
             <ul className='resume-text'>
               <li><i>Software Team Lead</i> for a Web-based, customer-facing e-commerce <a href='https://preptoyourdoor.netlify.app/' target='_blank' rel='noopener noreferrer'>platform</a>.</li>
               <li><strong>Languages</strong>: Largely written in <strong>Python</strong> and <strong>JavaScript.</strong></li>
               <li><strong>Web</strong>: Launched a <strong>React</strong> user interface and <strong>Flask</strong> REST APIs to facilitate data flow between the browser and the database.</li>
-              <li><strong>Database</strong>: Designed and developed a <strong>MySQL</strong> database.</li>
+              <li><strong>Databases</strong>: Designed and developed <strong>MySQL</strong> and <strong>DynamoDB</strong> databases.</li>
               <li><strong>Automation</strong>: Created <strong>Python</strong> and <strong>Bash</strong> scripts to automate user subscription renewal and deployed it to run on a <strong>Linux</strong> virtual machine on <strong>AWS EC2</strong>.</li>
               <li><strong>Cloud</strong>: Launched the platform through <strong>AWS Lambda</strong>, <strong>RDS</strong>, and <strong>S3</strong>.</li>
               <li><strong>Development</strong>: Performed code reviews to ensure high quality code across a cross-functional team of 10+ engineers.</li>
             </ul>
             <h6>
-              <strong>Creator</strong>, Grad Planner
+              <strong>Software Team Lead</strong>, Grad Planner
               <span className='resume-date-range'>9/19 - 4/20</span>
             </h6>
             <ul className='resume-text'>
               <li><i>Creator</i> of a web <a href='http://gradplanner.us/' target='_blank' rel='noopener noreferrer'>resource</a> that helps university students graduate on time.</li>
               <li><strong>Languages</strong>: Largely written in <strong>Python.</strong></li>
               <li><strong>Web</strong>: Created a <strong>Flask</strong> user interface and <strong>Python</strong> scheduling algorithm outputting optimized undergraduate plans in four or fewer years.</li>
-              <li><strong>Database</strong>: Developed and populated a <strong>MySQL</strong> database with class and degree data.</li>
+              <li><strong>Databases</strong>: Developed and populated a <strong>MySQL</strong> database with class and degree data.</li>
               <li><strong>Cloud</strong>: Launched the platform through <strong>AWS EC2</strong> and <strong>RDS</strong>.</li>
             </ul>
             <h6>
@@ -208,26 +208,15 @@ class Resume extends Component<Props> {
               <ul className='resume-text'>
                 <li>Written with <strong>React</strong> and <strong>TypeScript</strong>.</li>
               </ul>
-              <li><strong>Personal Website</strong>: Developed with <strong>Angular</strong>, supported by a <strong>GraphQL</strong> API written in <strong>Go</strong>.</li>
               <li><strong>CovidSweeper</strong>: A fun <a href='https://park-junha.github.io/CovidSweeper/' target='_blank' rel='noopener noreferrer'> Minesweeper variant</a> built with <strong>Angular</strong>, <strong>Node.js</strong>, and <strong>MongoDB</strong>.</li>
+              <li><strong>Personal Website</strong>: Developed with <strong>Angular</strong>, <strong>GraphQL</strong>, <strong>TypeScript</strong> and <strong>Go</strong>.</li>
             </ul>
             <h6>
               <strong>Automation Tools</strong>
             </h6>
             <ul className='resume-text'>
-              <li><strong>Spekkio</strong>: A set of services and APIs that automate CI/CD on GitHub developed with <strong>Go</strong>.</li>
+              <li><strong>Spekkio</strong>: A set of tools and services that automate CI/CD on GitHub. Written with <strong>Go</strong>.</li>
             </ul>
-            {/*
-            <h6>
-              <strong>Open Source Software</strong>
-            </h6>
-            <ul className='resume-text'>
-              <li><strong>Chrome V8</strong>: Google&apos;s JavaScript engine that powers Chrome and Node.js.</li>
-              <ul className='resume-text'>
-                <li>Implemented with <strong>C++</strong>.</li>
-              </ul>
-            </ul>
-            */}
           </div>
         </div>
       </div>
